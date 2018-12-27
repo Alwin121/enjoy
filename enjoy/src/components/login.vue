97bfbe8c0fcdaa71cda5f94b8b99f2e871ec54fc
xushaung <xushuang@163.com>
2018-12-27 17:30:54 +0800

    list

--- enjoy/src/components/login.vue
+++ enjoy/src/components/login.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-	login
+	
+
+	<input type="text" placeholder="用户名"><br>
+	<input type="text" placeholder="邮箱"><br>
+	<input type="password" placeholder="密码"><br>
+	<button>登录</button>
 	<router-view></router-view>    
   	
   </div>
@@ -12,5 +17,27 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+	input{
+		outline: none;
+		border: 1px solid #ccc;
+		width: 300px;
+		height: 40px;
+		margin-left: 20px;
+		margin-top: 20px;
+		padding-left: 20px;
+		border-radius: 5px;
+	}
+	input:nth-of-type(1){
+		margin-top: 60px
+	}
+	button{
+		width: 320px;
+		height: 50px;
+		margin-left: 20px;
+		margin-top: 20px;
+		background-color: #000;
+		color: #fff;
+		text-shadow:1px 1px 1px #333;
+		border: none;
+	}
 </style>
