93efc3585fb051234404261a88ac8b2416a242cd
Alwin <824826557@qq.com>
2018-12-27 19:36:08 +0800

    dongtailuyou

--- enjoy/src/components/index.vue
+++ enjoy/src/components/index.vue
@@ -66,7 +66,7 @@ methods:{
 		console.log(data.split("=")[2])
 		console.log(data.split("=")[1].split("&")[0])
 
-		this.$router.push(`/goods/${data.split("=")[1].split("&")[0]}/${data.split("=")[2]}`)
+		this.$router.push(`/goods/${data.split("=")[1].split("&")[0]}&${data.split("=")[2]}`)
 
 			}
 }
