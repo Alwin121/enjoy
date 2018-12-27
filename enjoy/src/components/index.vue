b5eb58fa83f8efa537446190d993f70290617d88
Alwin <824826557@qq.com>
2018-12-27 16:26:06 +0800

    waterfull

--- enjoy/src/components/index.vue
+++ enjoy/src/components/index.vue
@@ -1,47 +1,93 @@
 <template>
   <div>
-  	<div v-for="data in bigdetail">
+  	<ul v-infinite-scroll="loadMore"
+  		infinite-scroll-disabled="loading"
+  		infinite-scroll-immediate-check = "false"
+  		infinite-scroll-distance="0">
+  	<li class="bigli" v-for="data in bigdetail">
   		<h3>{{data.group_section.title}}</h3>
-		<p>{{data.group_section.desc}}</p>
+		<p class="bigp">{{data.group_section.desc}}</p>
   		<ul>
   			<li v-for="dl in data.tabs">	
   				<img :src="dl.url" alt="">
-  				<p>{{dl.desc}}</p>
-  				<p>{{dl.title}}</p>
+  				<p class="first">{{dl.desc}}</p>
+  				<p class="second">{{dl.title}}</p>
   			</li>
   		</ul>
-  	</div>
+  	</li>
+  </ul>
     <router-view></router-view>    
   </div>
 </template>
 
 <script>
 	import axios from "axios"
+	import { InfiniteScroll } from 'mint-ui';
 export default {
 	data(){
 		return{
-			bigdetail:[]
+			bigdetail:[],
+			loading:false,
+			page:0
 		}
 	},
 	mounted(){
 	axios({
-		url:'hub/home/v1/web/week_choice.json?city_id=140&page=0',
+		url:`hub/home/v1/web/week_choice.json?city_id=140&page=${this.page}`,
 		method:'get'
 	}).then(res=>{
-		console.log(res.data)
+		console.log(res)
 		this.bigdetail = res.data
 	}).catch(error=>{
 		console.log(error)
 	})
 },
+methods:{
 
+	loadMore(){
+		if (this.page === 7) {
+			this.loading = true
+		}
+		this.page++
+		axios({
+		url:`hub/home/v1/web/week_choice.json?city_id=140&page=${this.page}`,
+		method:'get'
+	}).then(res=>{
+		console.log(res)
+		this.bigdetail = [...this.bigdetail,...res.data]
 
+	}).catch(error=>{
+		console.log(error)
+	})
+
+	}
 }
-
+}
 </script>
 
 <style scoped>
 	img{
 		width: 100%;
+	}
+	p.bigp{
+		font-size: 12px;
+		color: red;
+		margin-bottom: 20px
+	} 
+	p,h3{
+		text-indent: 20px
+	}
+	.first{
+		font-size: 20px;
+		margin-top:20px 
+	}
+	.second{
+		font-size: 12px;
+		color: #ccc;
+		margin-bottom:20px 
+	}
+	h3{
+		margin-top:20px; 
+		font-size: 30px;
 	}
 </style>
