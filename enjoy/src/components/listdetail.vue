fbaf2bcc368d5f08d75db4c22203b572a0094ceb
caowei1995cw <caowei1995cw@163.com>
2018-12-28 19:59:33 +0800

    12.28 19:59提交

--- enjoy/src/components/listdetail.vue
+++ enjoy/src/components/listdetail.vue
@@ -1,50 +1,186 @@
 <template>
-  <div>
-	   <ul>
-			 <li><a href="">全部</a></li>
-			 <li><a href="">智能排序</a></li>
+	<div>
+		<div class="top">
+      <ul>
+			 <li>全部<span></span></li>
+			 <li @click="barClick()">{{init}}</li>
 	   </ul>
-  	<listdetail1></listdetail1>
-  </div>
+
+		 <div class="bar" v-show="isShow">
+       <div v-for="mylist in list.sort" :key="list.sort.sort_id" class="bg" @click="idClick(mylist.sort_id,mylist.sort_name)" :class="ids==mylist.sort_id?'cur':''">
+          {{mylist.sort_name}}
+         
+				</div>
+		 </div>
+		 
+		 
+		</div>
+
+		<div class="body" v-if="datalist">
+			<ul v-for="data in datalist" :key="datalist.merchant_id">
+                <li>
+                    <img :src="data.product_image">
+                </li>
+                <li>
+                    <p>{{data.name}}</p>
+                    <p>{{data.price/100}}元/张<s>￥{{data.original_price/100}}</s></p>
+                    <p>
+                        <i class="iconfont icon-map" v-if="data.area"></i>
+                        {{data.area}}</p>
+                </li>
+            </ul>
+		</div>
+		
+	</div>
 </template>
 
 <script>
-import listdetail1 from "./listdetail1"
-// import { Actionsheet } from 'mint-ui';
+import axios from "axios"
 export default {
   data(){
 		return {
-
+			datalist:[],
+			list:[],
+			isShow:false,
+			init:"智能排序",
+			ids:0
 		}
 	},
-	components:{
-  listdetail1
-	}
+	beforeMount(){
+      axios({
+        url:`https://api.ricebook.com/4/tab/category_product_list.json?category_id=${this.$route.params.enjoy}&sort=1&from_id=0&city_id=140&page=0`,
+        method: 'get'      
+      }).then(res=>{
+        // console.log(res.data)
+        // console.log(res)
+        this.datalist = res.data
 
+			})
+
+			axios({
+        url:`https://api.ricebook.com/4/tab/sub_category.json?category_id=${this.$route.params.enjoy}&city_id=140&from_id=0`,
+        method: 'get'      
+      }).then(res=>{
+        console.log(res.data)
+        // console.log(res)
+        this.list = res.data
+
+			})
+
+	
+	},
+	methods:{
+		barClick(){
+       this.isShow =! this.isShow
+		},
+		idClick(id,name){
+			this.init=name
+			axios({
+        url:`https://api.ricebook.com/4/tab/category_product_list.json?category_id=23&sort=${id}&from_id=0&city_id=140&page=0`,
+        method: 'get'      
+      }).then(res=>{
+        // console.log(res.data)
+        // console.log(res)
+				this.datalist = res.data
+				this.isShow =! this.isShow
+				this.ids = id
+
+
+			})
+		}
+	}
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-a{
-	 text-decoration:none;
-}
-div{
-	ul{
-		  height:40px;
-			display: flex;
-			border-bottom: 1px solid #ccc;
-		li{
-			line-height:40px;
-			flex: 1;
 
+.body{
+     ul{
+        display: flex;
+        margin: 10px;
+         li{
+             margin-right: 10px;
+            flex: 1;
+             img{
+                 width: 170px;
+                 height: 115px;
+             }
+             
+         }
+				 
+ li:nth-child(2){
+    
+            p:nth-child(1){
+                 font-size: 13px;
+                 height: 68px;
+             }
+            p:nth-child(2){
+                 font-size: 15px;
+                 color: red;
+                 s{
+                     color: #ccc;
+                     margin-left: 10px;
+                 }
+             }
+             p:nth-child(3){
+                 font-size: 8px;
+                 color: #666;
+                 line-height: 25px;
+                 i{
+                     font-size: 2px;
+                 }
+             }
+ }
+     }
+ }
+
+
+
+
+.top{
+     position: relative;
+  ul{
+		overflow: hidden;
+		li{
+			float: left;
+			width: 50%;
+			height: 50px;
+			line-height: 50px;
 			text-align: center;
+			border-bottom: 1px solid #ccc;
+			position: relative;
+			span{
+         display: inline-block;
+				 width: 1px;
+				 height: 25px;
+				 background: #ccc;
+				 position: absolute;
+				 top: 14px;
+				 right: 0;
+				 
+			}
 		
 		}
 	}
-}
-	li:nth-child(1) a{
-				color:#666;
-          border-right: 1px solid #ccc;
+	
+		.bar{
+        // background: #f60;
+				position: absolute;
+				top:50px;
+				width: 100%;
+				.bg{
+					
+					background: #fff;
+					height: 50px;
+					text-align: center;
+					line-height: 50px;
+					font-size: 14px;
+					border-bottom: 1px solid #ccc;
+				}
 			}
+}  
+.cur{
+	background: #ccc;
+	color: red;
+}      
 </style>
--- enjoy/src/components/listdetail1.vue
+++ enjoy/src/components/listdetail1.vue
@@ -7,13 +7,16 @@
                 </li>
                 <li>
                     <p>{{data.name}}</p>
-                    <p>价格:{{data.price}}</p>
-                    <p><span></span></p>
+                    <p>{{data.price/100}}元/张<s>￥{{data.original_price/100}}</s></p>
+                    <p>
+                        <i class="iconfont icon-map" v-if="data.area"></i>
+                        {{data.area}}</p>
                 </li>
             </ul>
         </div>
        
     </div>
+    
 </template>
 
 <script>
@@ -21,7 +24,8 @@ import axios from "axios"
 export default {
     data(){
         return {
-        datalist:[]
+        datalist:[],
+        
         }
     }
     ,
@@ -31,11 +35,16 @@ export default {
         url:`https://api.ricebook.com/4/tab/category_product_list.json?category_id=${this.$route.params.enjoy}&sort=1&from_id=0&city_id=140&page=0`,
         method: 'get'      
       }).then(res=>{
-        console.log(res.data)
-        console.log(res)
+        // console.log(res.data)
+        // console.log(res)
         this.datalist = res.data
+
+       
       })
-	}
+    },
+    method(){
+        
+    }
   }
 
 </script>
@@ -43,12 +52,41 @@ export default {
 <style scoped lang="scss">
  div{
      ul{
+        display: flex;
+        margin: 10px;
          li{
+             margin-right: 10px;
+            flex: 1;
              img{
-                 width: 173px;
+                 width: 170px;
                  height: 115px;
              }
+             
          }
      }
+ }
+
+ li:nth-child(2){
+    
+            p:nth-child(1){
+                 font-size: 13px;
+                 height: 68px;
+             }
+            p:nth-child(2){
+                 font-size: 15px;
+                 color: red;
+                 s{
+                     color: #ccc;
+                     margin-left: 10px;
+                 }
+             }
+             p:nth-child(3){
+                 font-size: 8px;
+                 color: #666;
+                 line-height: 25px;
+                 i{
+                     font-size: 2px;
+                 }
+             }
  }
 </style>
